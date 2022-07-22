0f07a9573e9b081659985a2ad88de8650d0a1818
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-07-22 18:53:45 +0100

    complete models

--- conficon/settings.py
+++ conficon/settings.py
@@ -125,7 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-
+# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_URL = "/files/"
+MEDIA_ROOT = BASE_DIR / "media/files"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- conficon_app/models.py
+++ conficon_app/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
                                         PermissionsMixin)
 from django.db import models
 
+from .utils import file_path
+
 
 class NewUserAccountManager(BaseUserManager):
     def create_superuser(self, username, email, password, **other_fields):
@@ -51,21 +53,33 @@ class Profile(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=False)
 
     objects = NewUserAccountManager()
-    USERNAME_FIELD = 'username'
+    USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["email"]
 
     def __str__(self):
         return self.username
+
+    class Meta:
+        ordering = ["username"]
 
 
 # The foreignKey is not what we want
 class Icon(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="files/icons")
+    image = models.ImageField(upload_to=file_path)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ["-created", "name"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "name"], name="unique_icon_per_user"
+            ),
+        ]
 
 
 class Result(models.Model):
@@ -76,8 +90,12 @@ class Result(models.Model):
 
     name = models.CharField(max_length=100)
     upload = models.OneToOneField(Icon, on_delete=models.CASCADE)
-    zip_file = models.FileField(upload_to="files/results")
+    zip_file = models.FileField(upload_to=file_path)
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ["-created", "name"]
