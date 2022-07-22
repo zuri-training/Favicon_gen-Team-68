f41450cd7a0ee30697f957a12f7ab2ade904e492
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-07-22 19:09:35 +0100

    add index.html stub file

--- conficon/settings.py
+++ conficon/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = "conficon.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- conficon/urls.py
+++ conficon/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("conficon_app.urls")),
 ]
 
 urlpatterns += static(
