dc0a4466da532ea7b33c9857d3c0ce1bb6f3adbe
vicky1312 <victoryabayomi@gmail.com>
2022-07-23 18:45:16 +0100

    Html login and registration

--- conficon_app/views.py
+++ conficon_app/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+def index(request):
+    return render(request, 'signup.html')
