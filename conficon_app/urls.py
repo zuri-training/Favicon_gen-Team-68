867c0a47965c180d9cf9eaf48879bfa9c42fd49d
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-07-27 15:44:15 +0100

    change to 3 page web app from 4

--- conficon_app/urls.py
+++ conficon_app/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("signup/", views.signup_view, name="signup"),
-    path("authorized-page", views.authorized_page, name="auth-page"),
 ]
