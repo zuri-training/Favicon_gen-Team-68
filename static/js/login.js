66002e926a3e4214056980cb05771f923bea3c75
tony <anthonyugwuja.dev@gmail.com>
2022-07-29 17:16:12 +0000

    updated login styles

--- static/js/login.js
+++ static/js/login.js
@@ -1,19 +1,18 @@
+const togglePassword = document.querySelector("#togglePassword");
 const togglePassword1 = document.querySelector("#hide1");
 const togglePassword2 = document.querySelector("#hide2");
 const password = document.querySelector("#password");
 
-togglePassword1.addEventListener("click", function () {
-   
+togglePassword.addEventListener("click", function () {
   // toggle the type attribute
-  if (password.type === 'password'){
-    password.type = 'text'
-    togglePassword1.style.display = block
-    togg
+  if (password.type === "password") {
+    password.type = "text";
+    togglePassword1.style.display = block;
+    togg;
   }
- 
 });
-        // prevent form submit
-        const form = document.querySelector("form");
-        form.addEventListener('submit', function (e) {
-            e.preventDefault();
-        });
+// prevent form submit
+const form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
