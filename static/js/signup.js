c4ddf6b39005c801c88f8b2e3e69eaa93c31e2f8
vicky1312 <victoryabayomi@gmail.com>
2022-07-30 21:56:42 +0100

    signup.js update

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,8 +1,19 @@
-const visibility = document.querySelector("#visibility");
-    const password = ("click", "#password");
+var togglePassword1 = document.querySelector("#hide1");
+var togglePassword2 = document.querySelector("#hide2");
+var password = document.querySelector("#password");
 
-visibility.addEventListener('click', function(e) {
-    const type = password.getAttribute('type')=== "password" ? "text" : "password";
-    password.setAttribute('type', type)
-    this.classList.toggle('fa-eye-slash');
+togglePassword1.addEventListener("click", function () {
+   
+  // toggle the type attribute
+  if (password.type === 'password'){
+    password.type = 'text'
+    togglePassword1.style.display = block
+    togg
+  }
+ 
 });
+        // prevent form submit
+        var form = document.querySelector("form");
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+        });
