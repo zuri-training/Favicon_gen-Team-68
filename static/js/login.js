aed22b1b498a3fb460060eb8e412f4c0cd642f49
tony <anthonyugwuja.dev@gmail.com>
2022-07-30 20:23:00 +0000

    Added a theme-color to the website, completed login styles and functionalities both Desktop and mobile view using 4 point system, Also css styles effects on hover, clicked, active and visited

--- static/js/login.js
+++ static/js/login.js
@@ -1,2 +1,20 @@
 
 
+    const toggle = document.querySelector(".eyes");
+    const password = document.querySelector("#password");
+    
+    toggle.addEventListener("click", () =>{
+      // toggle the type attribute
+      if (password.type === "password") {
+        password.type = "text";
+        toggle.classList.replace('fa-eye-slash', 'fa-eye')
+          }else{
+            password.type = 'password'
+            toggle.classList.replace('fa-eye', 'fa-eye-slash')
+          }
+    });
+
+  const login = document.getElementById('submit')
+  login.addEventListener('click', ()=>{
+    login.textContent = 'Authenticating....'
+  })
