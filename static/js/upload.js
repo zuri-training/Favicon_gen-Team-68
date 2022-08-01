edff759b48fac536903f789e08c759e7da24e1f6
tony <anthonyugwuja.dev@gmail.com>
2022-08-01 23:40:22 +0000

    Done styling upload page, remaining some click functionality

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,12 +1,30 @@
-
-const darkMode = document.querySelector('.child2')
 const lightMode = document.querySelector('.child1')
-const prevModalVDiv = document.querySelector('prev-board')
+const prevModalVDiv = document.querySelector('.prev-board')
 darkMode.addEventListener('click', ()=>{
-    prevModalVDiv.style.backgroundColor = '#323337'
     lightMode.classList.remove('active-toggle')
     darkMode.classList.add('active-toggle')
+prevModalVDiv.style.backgroundColor = '#323337'
    
 })
+lightMode.addEventListener('click' , ()=>{
+    lightMode.classList.add('active-toggle')
+    darkMode.classList.remove('active-toggle')
+    prevModalVDiv.style.backgroundColor = '#dddddd'
+})
 
+const modal = document.querySelector('#modal')
+const closeButton = document.querySelector('.close-modal');
+
+const closeModal = () =>{
+    modal.classList.add('hidden')
+        }
+ closeButton.addEventListener('click', closeModal )
+ document.body.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', (e)=>{
+    // console.log(e.key)
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModal();
+    }
+})
 
