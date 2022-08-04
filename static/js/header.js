55ee7f5e52072f384a4d1b78ff4a2b1d0a7263f7
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-08-04 17:04:50 +0100

    add cool preview to image uploaded

---
+++ static/js/header.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("upload");
+const modal = document.getElementById("modal");
+const overlay = document.querySelector(".overlay");
+const fileName = document.getElementById("file-name");
+const input = document.getElementById("file-input");
+
+const displayImage = (e) => {
+  while (fileName.firstChild) {
+    fileName.removeChild(fileName.firstChild);
+  }
+  const image = document.createElement("img");
+  const div = document.createElement("div");
+  div.textContent = e.target.files[0].name;
+  image.src = URL.createObjectURL(e.target.files[0]);
+  image.style.marginTop = "20px";
+  image.style.maxWidth = "200px";
+  fileName.appendChild(image);
+  fileName.appendChild(div);
+};
+
+const generateIcon = (e) => {
+  e.preventDefault();
+  const http = new XMLHttpRequest();
+  const fd = new FormData(e.target);
+  http.addEventListener("load", (ev) => {
+    modal.style.display = "flex";
+  });
+  if (fd.get("file-input").size > 10e6) {
+    alert("file size exceeded 10 mb");
+    return;
+  }
+  http.addEventListener("error", (event) => {
+    alert("Oops failed to upload");
+  });
+  http.open("POST", location.origin + "/upload/", true);
+  http.send(fd);
+};
+
+input.addEventListener("change", displayImage);
+form.addEventListener("submit", generateIcon);
