3afc5d1f2acea9055b6f2a0dd37ba05888c8b943
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-08-08 05:19:38 +0100

    edit upload

--- static/js/upload.js
+++ static/js/upload.js
@@ -7,21 +7,23 @@ darkMode.addEventListener("click", () => {
   lightMode.classList.remove("active-toggle");
   darkMode.classList.add("active-toggle");
   prevModalVDiv.style.backgroundColor = "#232330";
-  codeDiv.style.backgroundColor = "#232330";
-  codeDiv.style.color = "#ffffff";
+ 
 });
 lightMode.addEventListener("click", () => {
   lightMode.classList.add("active-toggle");
   darkMode.classList.remove("active-toggle");
-  prevModalVDiv.style.backgroundColor = "#ddd";
-  codeDiv.style.backgroundColor = "#ddd";
+  prevModalVDiv.style.backgroundColor = "#ffffff";
+  
 });
 //closing the modal of upload page
-const closeButton = document.querySelector(".close-modal");
+
+
 const closeModal = () => {
   modal.style.display = "none";
+  closeBtnDesk.classList.add('hidden')
 };
 closeButton.addEventListener("click", closeModal);
+closeBtnDesk.addEventListener("click", closeModal);
 document.addEventListener("keydown", (e) => {
   // console.log(e.key)
   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
@@ -29,20 +31,20 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-//const resultForm = document.getElementById("result-gen");
-//resultForm.addEventListener("submit", (e) => {
-//e.preventDefault();
-//});
+const resultForm = document.getElementById("result-gen");
+resultForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 //Copying embedded html code for the generated favicon
 function CopyToClipboard(containerId) {
   if (document.selection) {
     let range = document.body.createTextRange();
-    range.moveToElementText(document.querySelector(".cp-space"));
+    range.moveToElementText(document.querySelector(".prettyprint"));
     range.select().createTextRange();
     navigator.clipboard.writeText(range);
   } else if (window.getSelection) {
     let range = document.createRange();
-    range.selectNode(document.querySelector(".cp-space"));
+    range.selectNode(document.querySelector(".prettyprint"));
     window.getSelection().removeAllRanges();
     window.getSelection().addRange(range);
     navigator.clipboard.writeText(range);
@@ -52,29 +54,28 @@ function CopyToClipboard(containerId) {
 //Adding embedded html code from the user selection of preferred sizes
 let checkboxes = document.querySelectorAll("input[type=checkbox]");
 
-const insertDiv = document.getElementById("box");
+let insertDiv = document.querySelector(".prettyprint");
 
 /*  function func() {
       checkboxes[1].checked = true;
   } */
+
+
 
 for (let i = 0; i < checkboxes.length; i++) {
   checkboxes[i].addEventListener("click", function (i) {
     let value = i.target.value;
     let iconType = i.target.classList.value;
     let checked = i.target.checked;
+    let valueSet = `${value}x${value}`
     console.log(value, iconType, checked);
-    let html2 = ` &lt;link rel="manifest" href="/site.webmanifest"&gt; `;
-    let html3 = `<code class="prettyprint"> &lt;link rel="apple-touch-icon" sizes="${i.value}x${i.value}" href="/apple-touch-icon.png"&gt; </code>`;
-    let html1 = `<code class="prettyprint"> &lt;link rel="icon" type="image/png" sizes="${value}x${value}" href="/favicon-${value}x${value}.png"&gt; </code>`;
+   
     if ((checked = true)) {
-      if (iconType == "size-icon") {
-        insertDiv.insertAdjacentHTML("afterbegin", html1 + "\n");
-      } else if (iconType == "mani") {
-        insertDiv.insertAdjacentHTML("afterbegin", html2 + "\n");
-      } else if (iconType == "apple") {
-        insertDiv.insertAdjacentHTML("afterbegin", html3 + "\n");
-      }
+      let sizes = ['16x16']
+      let faviconLink = `<link rel="icon" type="images/x-icon" sizes="${sizes}" href="/favicon.ico">`
+     sizes.push(valueSet)
+     console.log(sizes.join(' '));
+     insertDiv.textContent = faviconLink
     } else if ((checked = false)) {
       insertDiv.textContent = "";
     }
