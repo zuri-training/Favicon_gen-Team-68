e091953fb2e106ee24ead26121f6010f2d6d8c8f
NPDebs <deborahudoh02@gmail.com>
2022-08-11 23:01:58 -0700

    Merge all files

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -3,12 +3,14 @@ const navMenu = document.querySelector(".nav-menu");
 const genDropDown = document.querySelector('.drop-down');
 const generate = document.
 getElementById('nav-generate');
+const acc = document.querySelector('.acc')
+const accDropDown = document.querySelector('.drop-down-2"')
 // const bar = document.getElementById('bar');
 const activeUpload = document.querySelector('.up');
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
+// hamburger.addEventListener("click", () => {
+//   hamburger.classList.toggle("active");
+//   navMenu.classList.toggle("active");
+// });
 
 document.querySelectorAll(".nav-link").forEach((n) =>
   n.addEventListener("click", () => {
@@ -16,14 +18,21 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     navMenu.classList.remove("active");
   })
 );
+
 const genDrop = ()=>{
   genDropDown.style.display = 'flex'  
 }
-generate.addEventListener("hover", genDrop);
+
 generate.addEventListener("click", genDrop);
+generate.addEventListener("hover", genDrop);
 activeUpload.addEventListener("hover", function(){
   activeUpload.classList.toggle("active-upload");
 });
 activeUpload.addEventListener("click", function(){
   activeUpload.classList.add("active-upload");
 });
+const displayAcc = () =>{
+accDropDown.style.display = 'flex'
+}
+acc.addEventListener('click', displayAcc)
+acc.addEventListener('hover', displayAcc)
